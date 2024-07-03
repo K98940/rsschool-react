1a5b435f37cb1c36be11f473b3e9bd6dcad0114a
k98940 <pv1xxb@yandex.ru>
2024-07-03 18:20:07 +0600

    feat: add component Header

---
+++ src/components/header/header.tsx
@@ -0,0 +1,56 @@
+import classes from './header.module.css';
+import { Component, ReactNode } from 'react';
+import { Status } from '@components/state/state';
+import DamageButton from '../damageButton/damageButton';
+
+export type SubmitEvents = React.MouseEvent | React.FormEvent;
+
+type HeaderProps = {
+  status: Status;
+  handleSearch: (e: SubmitEvents, term: string) => void;
+};
+
+export default class Header extends Component<HeaderProps> {
+  state = {
+    term: 'Albatross',
+  };
+
+  handleTermChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    this.setState({ ...this.state, term: e.target.value });
+  };
+
+  handleTermReset = (): void => {
+    this.setState({ ...this.state, term: '' });
+  };
+
+  render(): ReactNode {
+    const classSearchContainer =
+      this.props.status === 'submitting'
+        ? `${classes.searchContainer} ${classes.disabledElement}`
+        : classes.searchContainer;
+
+    return (
+      <header className={classes.header}>
+        <div className={classSearchContainer}>
+          <DamageButton text="Do Damage" />
+          <form className={classes.inputContainer} onSubmit={(e) => this.props.handleSearch(e, this.state.term)}>
+            <input
+              className={classes.input}
+              onChange={this.handleTermChange}
+              value={this.state.term}
+              placeholder="search animal"
+            ></input>
+            {this.state.term.length > 0 && (
+              <button type="reset" className={classes.btnReset} onClick={this.handleTermReset}>
+                x
+              </button>
+            )}
+          </form>
+          <button className={classes.btnSearch} onClick={(e) => this.props.handleSearch(e, this.state.term)}>
+            Search
+          </button>
+        </div>
+      </header>
+    );
+  }
+}

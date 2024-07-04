d18ea2d8b21c3c9413400f43df661a8fc277451b
k98940 <pv1xxb@yandex.ru>
2024-07-04 14:07:40 +0600

    refactor: lifting state up from Header into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,44 @@
 import './App.css';
 import Api from '@api/api';
 import Main from '@components/main/main';
+import { ls } from './helpers/localStorage';
 import { Component, ReactNode } from 'react';
 import Header from '@components/header/header';
+import { AppState, Status } from './types/types';
 import Spinner from '@components/spinner/spinner';
-import { isAnimalBaseResponse } from '@helpers/predicates';
-import { AppState, Status, SubmitEvents } from './types/types';
+import { initialState } from '@/helpers/constants';
+import { isEpisodeBaseResponse } from '@helpers/predicates';
 import ErrorBoundary from '@components/errorBoundary/errorBoundary';
 
 const api = new Api();
 
 class App extends Component {
-  state: AppState = {
-    status: 'idle',
-    data: null,
-  };
+  localStorageData: string | null = '';
+  state: AppState = initialState;
 
   updateStatus = (newStatus: Status): void => {
     this.setState((prevState) => ({ ...prevState, status: newStatus }));
   };
 
-  handleSearch = (e: SubmitEvents, term: string): void => {
-    e.preventDefault();
-    console.log('search', term);
+  handleQueryChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    this.setState({ ...this.state, query: e.target.value });
+  };
+
+  handleSearch = (): void => {
     if (this.state.status === 'submitting') return;
+    this.getData(this.state.query);
+  };
+
+  handleQueryReset = (): void => {
+    this.setState((prevState) => ({ ...prevState, query: '' }));
+  };
+
+  getData(term: string) {
     this.updateStatus('submitting');
     api
-      .searchAnimal(term)
+      .searchEpisode(term)
       .then((resp) => {
-        if (isAnimalBaseResponse(resp)) {
+        if (isEpisodeBaseResponse(resp)) {
           this.setState((prevState) => ({ ...prevState, data: resp }));
         } else {
           console.log(`this is a bad response (code: ${resp.status}) `, resp.statusText);
@@ -38,13 +48,12 @@ class App extends Component {
       .finally(() => {
         this.updateStatus('idle');
       });
-  };
+  }
 
-  componentDidUpdate(_: never, prevState: Readonly<AppState>): void {
-    const cState = JSON.stringify(this.state.data);
-    const pState = JSON.stringify(prevState.data);
-    if (cState !== pState) {
-      console.log('NEW STATE!', new Date().toLocaleTimeString());
+  componentDidMount(): void {
+    const localStorageData = ls.get();
+    if (localStorageData) {
+      this.setState((prevState) => ({ ...prevState, query: localStorageData }));
     }
   }
 
@@ -53,8 +62,14 @@ class App extends Component {
       <>
         <ErrorBoundary>
           {this.state.status === 'submitting' && <Spinner />}
-          <Header handleSearch={this.handleSearch} status={this.state.status} />
-          <Main data={this.state.data?.animals} />
+          <Header
+            query={this.state.query}
+            handleSearch={this.handleSearch}
+            handleQueryChange={this.handleQueryChange}
+            handleQueryReset={this.handleQueryReset}
+            status={this.state.status}
+          />
+          <Main data={this.state.data?.episodes} />
         </ErrorBoundary>
       </>
     );
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,25 +1,20 @@
-import { Component, ReactNode } from 'react';
+import { Status } from '@/types/types';
 import classes from './header.module.css';
-import { Status, SubmitEvents } from '@/types/types';
+import { Component, createRef, ReactNode } from 'react';
 import DamageButton from '../damageButton/damageButton';
 
 type HeaderProps = {
+  query: string;
   status: Status;
-  handleSearch: (e: SubmitEvents, term: string) => void;
+  handleSearch: () => void;
+  handleQueryChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  handleQueryReset: () => void;
 };
 
 export default class Header extends Component<HeaderProps> {
-  state = {
-    term: 'Albatross',
-  };
+  private readonly ref = createRef<HTMLInputElement>();
 
-  handleTermChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    this.setState({ ...this.state, term: e.target.value });
-  };
-
-  handleTermReset = (): void => {
-    this.setState({ ...this.state, term: '' });
-  };
+  // componentDidUpdate(prevProps: Readonly<HeaderProps>): void {}
 
   render(): ReactNode {
     const classSearchContainer =
@@ -31,20 +26,41 @@ export default class Header extends Component<HeaderProps> {
       <header className={classes.header}>
         <div className={classSearchContainer}>
           <DamageButton text="Do Damage" />
-          <form className={classes.inputContainer} onSubmit={(e) => this.props.handleSearch(e, this.state.term)}>
+          <form
+            className={classes.inputContainer}
+            onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
+              e.preventDefault();
+              this.props.handleSearch();
+            }}
+          >
             <input
               className={classes.input}
-              onChange={this.handleTermChange}
-              value={this.state.term}
-              placeholder="search animal"
+              onChange={this.props.handleQueryChange}
+              value={this.props.query}
+              placeholder="Search for the Star Trek episode"
+              autoFocus
+              ref={this.ref}
             ></input>
-            {this.state.term.length > 0 && (
-              <button type="reset" className={classes.btnReset} onClick={this.handleTermReset}>
+            {this.props.query.length > 0 && (
+              <button
+                type="reset"
+                className={classes.btnReset}
+                onClick={() => {
+                  this.ref.current?.focus();
+                  this.props.handleQueryReset();
+                }}
+              >
                 x
               </button>
             )}
           </form>
-          <button className={classes.btnSearch} onClick={(e) => this.props.handleSearch(e, this.state.term)}>
+          <button
+            className={classes.btnSearch}
+            onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
+              e.preventDefault();
+              this.props.handleSearch();
+            }}
+          >
             Search
           </button>
         </div>

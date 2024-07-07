1065beb1e7fffabd70030a7228e4564913065711
k98940 <pv1xxb@yandex.ru>
2024-07-07 20:38:33 +0600

    refactor: ErrorBoundary, add constants and componentDidCatch

--- src/components/errorBoundary/errorBoundary.tsx
+++ src/components/errorBoundary/errorBoundary.tsx
@@ -6,29 +6,29 @@ type ErrorBoundaryProps = {
   children?: ReactNode;
 };
 
+const ERROR_MESSAGE_TITLE = 'Something went wrong';
+const ERROR_MESSAGE_BOTTOM = 'But we`ll fix it soon';
+
 export default class ErrorBoundary extends Component<ErrorBoundaryProps> {
   state = {
     hasError: false,
   };
 
   static getDerivedStateFromError() {
-    console.log('Something terrible has happened. We should do something!');
     return { hasError: true };
   }
 
-  componentDidUpdate(prevProps: Readonly<ErrorBoundaryProps>): void {
-    if (this.props.children !== prevProps.children) {
-      this.setState({ hasError: false });
-    }
+  componentDidCatch(error: Error): void {
+    console.log(error);
   }
 
   render(): ReactNode {
     if (this.state.hasError) {
       return (
         <div className={classes.boundaryContainer}>
-          <h1 className={classes.header}>Something went wrong</h1>
-          <img src={imgBroken} alt="Something went wrong" />
-          <p className={classes.content}>But we'll fix it soon</p>
+          <h1 className={classes.header}>{ERROR_MESSAGE_TITLE}</h1>
+          <img src={imgBroken} alt={ERROR_MESSAGE_TITLE} />
+          <p className={classes.content}>{ERROR_MESSAGE_BOTTOM}</p>
         </div>
       );
     }

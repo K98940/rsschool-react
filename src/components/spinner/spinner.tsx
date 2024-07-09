91a6fdd473c3484f863533b1d600fb0d600a8b7b
k98940 <pv1xxb@yandex.ru>
2024-07-09 23:37:43 +0600

    refactor: translate spinner.tsx class component into functional

--- src/components/spinner/spinner.tsx
+++ src/components/spinner/spinner.tsx
@@ -1,13 +1,10 @@
 import classes from './spinner.module.css';
-import { Component, ReactNode } from 'react';
 import imgSpinner from '@assets/icons/spinner.webp';
 
-export default class Spinner extends Component {
-  render(): ReactNode {
-    return (
-      <div className={classes.spinner}>
-        <img className={classes.spinnerImg} src={imgSpinner} width={64} height={64} alt="loading..." />
-      </div>
-    );
-  }
+export default function Spinner() {
+  return (
+    <div className={classes.spinner}>
+      <img className={classes.spinnerImg} src={imgSpinner} width={64} height={64} alt="loading..." />
+    </div>
+  );
 }

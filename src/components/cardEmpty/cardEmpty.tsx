aa697da3fc2ed3ce649c6e64ef47f0e3f377f275
k98940 <pv1xxb@yandex.ru>
2024-07-10 00:13:29 +0600

    refactor: translate cardEmpty.tsx class component into functional

--- src/components/cardEmpty/cardEmpty.tsx
+++ src/components/cardEmpty/cardEmpty.tsx
@@ -1,17 +1,12 @@
 import classes from './cardEmpty.module.css';
-import { Component, ReactNode } from 'react';
 
-class CardEmpty extends Component {
-  render(): ReactNode {
-    return (
-      <section className={classes.container}>
-        <div className={classes.card}>
-          <header className={classes.cardHeader}>Found nothing</header>
-          <p className={classes.content}>∅</p>
-        </div>
-      </section>
-    );
-  }
+export default function CardEmpty() {
+  return (
+    <section className={classes.container}>
+      <div className={classes.card}>
+        <header className={classes.cardHeader}>Found nothing</header>
+        <p className={classes.content}>∅</p>
+      </div>
+    </section>
+  );
 }
-
-export default CardEmpty;

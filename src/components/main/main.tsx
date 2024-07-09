91f506993b0925ba898810cf9d7e8d6f4c2af3c6
k98940 <pv1xxb@yandex.ru>
2024-07-09 23:43:22 +0600

    refactor: translate main.tsx class component into functional

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -1,23 +1,18 @@
 import Card from '../card/card';
 import classes from './main.module.css';
 import { EpisodeBase } from '@/types/types';
-import { Component, ReactNode } from 'react';
 import CardEmpty from '../cardEmpty/cardEmpty';
 
 type MainProps = {
-  data: EpisodeBase[] | undefined;
+  episodes: EpisodeBase[] | undefined;
 };
 
-export default class Main extends Component<MainProps> {
-  render(): ReactNode {
-    if (this.props.data?.length === 0)
-      return (
-        <main className={classes.main}>
-          <CardEmpty />
-        </main>
-      );
+export default function Main({ episodes }: MainProps) {
+  if (episodes?.length === 0)
     return (
-      <main className={classes.main}>{this.props.data?.map((episode, i) => <Card data={episode} key={i} />)}</main>
+      <main className={classes.main}>
+        <CardEmpty />
+      </main>
     );
-  }
+  return <main className={classes.main}>{episodes?.map((episode, i) => <Card data={episode} key={i} />)}</main>;
 }

d4f60aad8e38fbc74c0d0a0d4ede4653f5139bb2
k98940 <pv1xxb@yandex.ru>
2024-07-11 20:32:54 +0600

    feat: component Card: add button Close. Change source of data, now is Router

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,29 +1,38 @@
 import classes from './card.module.css';
-import { EpisodeBase } from '@/types/types';
+import CardEmpty from '../cardEmpty/cardEmpty';
+import { APP_ROOT_URL } from '@/helpers/constants';
+import { isEpisodeFullResponse } from '@/helpers/predicates';
+import { useLoaderData, useNavigate } from 'react-router-dom';
 
-export type CardProps = {
-  episode: EpisodeBase;
-};
+export default function Card() {
+  const data = useLoaderData();
+  const navigate = useNavigate();
 
-export default function Card({ episode }: CardProps) {
-  return (
-    <section className={classes.container}>
-      <div className={classes.card}>
-        <header className={classes.cardHeader} title={episode.title}>
-          {episode.title}
-        </header>
-        <div className={classes.contentContainer}>
-          <p className={classes.content}>
-            <strong>Season:</strong> {episode.season.title}
-          </p>
-          <p className={classes.content}>
-            <strong>Series:</strong> {episode.series.title}
-          </p>
-          <p className={classes.content}>
-            <strong>Date:</strong> {episode.usAirDate}
-          </p>
+  if (isEpisodeFullResponse(data)) {
+    const { episode } = data;
+    if (!episode) return <CardEmpty />;
+    return (
+      <section className={classes.container}>
+        <div className={classes.card}>
+          <header className={classes.cardHeader} title={episode.title}>
+            {episode.title}
+          </header>
+          <div className={classes.contentContainer}>
+            <p className={classes.content}>
+              <strong>Season:</strong> {episode.season.title}
+            </p>
+            <p className={classes.content}>
+              <strong>Series:</strong> {episode.series.title}
+            </p>
+            <p className={classes.content}>
+              <strong>Date:</strong> {episode.usAirDate}
+            </p>
+          </div>
+          <button className={classes.btnCloseCard} onClick={() => navigate(APP_ROOT_URL)}>
+            Close
+          </button>
         </div>
-      </div>
-    </section>
-  );
+      </section>
+    );
+  }
 }

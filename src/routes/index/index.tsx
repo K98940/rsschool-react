c9626ed43654e3fcb3244cf2a1fe6262d0099835
k98940 <pv1xxb@yandex.ru>
2024-07-11 20:36:13 +0600

    feat: add component Index

---
+++ src/routes/index/index.tsx
@@ -0,0 +1,9 @@
+import classes from './index.module.css';
+
+export default function Index() {
+  return (
+    <section className={classes.container}>
+      <h1 className={classes.title}>Select an episode...</h1>
+    </section>
+  );
+}

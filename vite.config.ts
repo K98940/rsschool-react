6c79ba1b45125e4d730ad3454c2afb99001c5a97
k98940 <pv1xxb@yandex.ru>
2024-07-12 21:50:17 +0600

    chore: install testing libraries

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
@@ -15,5 +16,8 @@ export default defineConfig({
       '@helpers': path.resolve(__dirname, './src/helpers'),
       '@components': path.resolve(__dirname, './src/components'),
     },
+  },
+  test: {
+    environment: 'jsdom',
   },
 });

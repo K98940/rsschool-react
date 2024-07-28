39023ebea3415a1e1900a9db57b131257632e144
k98940 <pv1xxb@yandex.ru>
2024-07-28 15:38:06 +0600

    fix: createCSV: add header, change separator

--- src/helpers/createCSV.ts
+++ src/helpers/createCSV.ts
@@ -1,6 +1,8 @@
 import { EpisodeBase } from '@/types/types';
 
-export default (episodes: EpisodeBase[]): string[] =>
-  episodes.map(
-    (e) => `episode title: ${e.title}; episode season: ${e.season.title}; series title: ${e.series.title}\n`,
-  );
+const separator = ';';
+const header = 'episode title;episode season;series title\n';
+const quoted = (s: string) => `"${s}"${separator}`;
+const getRow = (e: EpisodeBase) => `${quoted(e.title)}${quoted(e.season.title)}${quoted(e.series.title)}\n`;
+
+export default (episodes: EpisodeBase[]): string[] => [header, ...episodes.map((episode) => getRow(episode))];

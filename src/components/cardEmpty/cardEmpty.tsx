7e804f1923c31792446a88c701c13afdc10cbb26
k98940 <pv1xxb@yandex.ru>
2024-07-30 01:22:31 +0600

    refactor: CardEmpty - remove redundant code and styles

--- src/components/cardEmpty/cardEmpty.tsx
+++ src/components/cardEmpty/cardEmpty.tsx
@@ -3,12 +3,7 @@ import classes from './cardEmpty.module.css';
 export default function CardEmpty() {
   return (
     <section className={classes.container}>
-      <div className={classes.card}>
-        <header className={classes.cardHeader} data-testid="cardempty-header">
-          Found nothing
-        </header>
-        <p className={classes.content}>∅</p>
-      </div>
+      <h1 className={classes.cardHeader}>Select an episode...</h1>
     </section>
   );
 }

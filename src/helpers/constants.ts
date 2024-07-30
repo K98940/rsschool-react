d6d77bbbc0cc01e0399c2939fb6c2a4fe77b249d
k98940 <pv1xxb@yandex.ru>
2024-07-30 20:20:49 +0600

    chore: remove redundant files and constants

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,4 +1,4 @@
-import { AppState, EpisodeBase, Pagination } from '@/types/types';
+import { Pagination } from '@/types/types';
 
 export const ResponseKeys = ['headers', 'ok', 'status', 'statusText', 'type', 'url', 'redirected'];
 export const EpisodeFullKeys = ['uid', 'title', 'series', 'season', 'usAirDate'];
@@ -11,39 +11,8 @@ export const paths = {
   episode: '/v1/rest/episode?uid=',
 };
 export const localStorageKey = 'K1vjGYuKHzp4ZI9Xsrgq';
-export const initialState: AppState = {
-  query: '',
-  status: 'idle',
-};
-export const APP_URL_EPISODE = 'episode/';
-export const APP_URL_ROOT = '/';
 export const APP_URL_ROOT_PROD = '/rsschool-react/';
 export const PAGE_SIZE = 10;
-export const test_episode: EpisodeBase = {
-  episodeNumber: 1,
-  featureLength: 2,
-  finalScriptDate: '2024',
-  productionSerialNumber: 123450,
-  season: {
-    title: 'sdfsdf',
-    uid: '345345',
-  },
-  seasonNumber: 456,
-  series: {
-    title: 'fgjghj',
-    uid: '678789',
-  },
-  stardateFrom: 2,
-  stardateTo: 3,
-  title: 'test episode title',
-  titleGerman: 'dfg',
-  titleItalian: 'ghj',
-  titleJapanese: 'hjk',
-  uid: '65868j',
-  usAirDate: 'ghjfh',
-  yearFrom: 3,
-  yearTo: 55,
-};
 export const initialPagination: Pagination = {
   pageNumber: 0,
   pageSize: 0,

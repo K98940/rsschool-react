324c66cb25b63840fefddc17495178a3460d1d21
k98940 <pv1xxb@yandex.ru>
2024-08-04 23:51:10 +0600

    refactor: change navElement for App router

--- src/components/navElement/navElement.tsx
+++ src/components/navElement/navElement.tsx
@@ -1,17 +1,22 @@
 import Link from 'next/link';
 import { EpisodeBase } from '@/types/types';
 import classes from './navElement.module.css';
-import useGetParams from '@/hooks/useGetParams';
 import { EpisodeCheckbox } from '../episodeCheckbox/episodeCheckbox';
 
 type NavElementProps = {
+  params: { page: string; episode: string };
   episode: EpisodeBase;
+  searchParams: { [key: string]: string | string[] | undefined };
 };
-export default function NavElement({ episode }: NavElementProps) {
-  const { pageNumber, id } = useGetParams();
-  const href = `/page/${pageNumber}/episode/${episode.uid}`;
-  const classList = `${classes.navElement} ${id === episode.uid ? classes.active : ''}`;
+export default function NavElement({ episode, searchParams, params }: NavElementProps) {
+  const { page, episode: id } = params;
+  let href = `/page/${page}/episode/${episode.uid}`;
+  if (searchParams && 'search' in searchParams) {
+    const search = searchParams?.search;
+    if (search) href += `?search=${search}`;
+  }
 
+  const classList = `${classes.navElement} ${id === episode.uid ? classes.active : ''}`;
   return (
     <li className={classList}>
       <EpisodeCheckbox episode={episode} />

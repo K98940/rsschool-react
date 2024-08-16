ebc8f85eb3cc04fc81dc31a461628fd7b84850b3
k98940 <pv1xxb@yandex.ru>
2024-08-16 22:40:42 +0600

    init: initial commit

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/rsschool-react/forms',
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@api': path.resolve(__dirname, './src/api'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@routes': path.resolve(__dirname, './src/routes'),
+      '@helpers': path.resolve(__dirname, './src/helpers'),
+      '@components': path.resolve(__dirname, './src/components'),
+    },
+  },
+  test: {
+    environment: 'jsdom',
+  },
+});
